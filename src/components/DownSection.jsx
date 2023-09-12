9904466bc79640357d99a456c15cf7e0bb4711a4
unknown <nur.mnzaman@gmail.com>
2023-09-12 17:48:47 +0600

    [feat] app structure

---
+++ src/components/DownSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DownSection = () => {
+  return (
+    <div>DownSection</div>
+  )
+}
+
+export default DownSection
---
+++ src/components/Weather.jsx
@@ -0,0 +1,15 @@
+import Search from './Search'
+import MiddleSection from './MiddleSection'
+import DownSection from './DownSection'
+
+const Weather = () => {
+  return (
+    <div>
+        <Search />
+        <MiddleSection />
+        <DownSection />
+    </div>
+  )
+}
+
+export default Weather
