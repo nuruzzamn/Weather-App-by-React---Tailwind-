51f47aae668492956db90796f5e70d0ed5bbac2e
unknown <nur.mnzaman@gmail.com>
2023-09-14 10:35:54 +0600

    [feat] api fetch

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,11 @@
 import SearchCard from "./SearchCard";
 import { useState, useEffect } from "react";
-import useFetch from "../hook/useFetch";
 
 const Search = () => {
   const [searchData, setSearchData] = useState("");
-  const [newData, setNewData] = useState("");
+
+  const [fetchData, setFetchData] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const api =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -15,31 +16,35 @@ const Search = () => {
     setSearchData(e.target.value);
   };
 
+  const apiDataHandle = () => {
+    if (searchData) {
+      fetch(api)
+        .then((res) => {
+          if (!res.ok) {
+            throw "Finding error!! Enter a valid location";
+          } else {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          setFetchData(data);
+        })
+        .catch((error) => {
+          setErrorMessage(error);
+        });
+    } else {
+      console.log("enter a location name");
+    }
+  };
+
   const searchClickHandle = () => {
     setSearchData("");
-    setNewData(searchData);
 
-    if(searchData){
-      fetch(api)
-      .then((res) => {
-
-        console.log(res)
-
-        if (!res.ok) {
-          throw ('Finding error');
-        } else {
-          return res.json();
-        }
-      })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    }
-
+    apiDataHandle();
   };
+
+  fetchData && console.log("fetch data", fetchData);
+  errorMessage && console.log(errorMessage);
 
   return (
     <div>
