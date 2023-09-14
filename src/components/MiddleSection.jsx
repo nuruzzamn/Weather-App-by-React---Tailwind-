df0e153f33ee79fcfecb8952e84cf7cd89e6a203
unknown <nur.mnzaman@gmail.com>
2023-09-15 04:31:26 +0600

    [feat] temp data fetch and implement

--- src/components/MiddleSection.jsx
+++ src/components/MiddleSection.jsx
@@ -1,20 +1,29 @@
-import skySun from "../assets/icons/sky-sun.png"
-const MiddleSection = ({middleData}) => {
-  const {des, temp, name}=middleData
+import skySun from "../assets/icons/sky-sun.png";
+const MiddleSection = ({ middleData }) => {
+  const { des, temp, name } = middleData;
 
   return (
     <div className="">
       <section className="max-w-xs flex flex-row flex-1 justify-between">
         <section className="basis-4/6">
-            <img src={skySun} alt="sky-sun" className="sm:w-56 sm:h-56 w-36 h-36"/>
+          <img
+            src={skySun}
+            alt="sky-sun"
+            className="sm:w-56 sm:h-56 w-36 h-36"
+          />
         </section>
 
         <section className="basis-2/6 font-montserrat text-slate-700 pl-3 mt-4 ">
-            <h1 className="text-lg">{des}</h1>
-            {temp && <p className="text-4xl sm:text-8xl mt-2">{Math.round(temp)}</p>}
-            <h2 className="text-xs sm:text-lg font-bold">{name}</h2>
-        </section>
+          <h1 className="text-lg">{des}</h1>
+          {temp && (
+            <section className="flex ">
+              <p className="text-4xl sm:text-8xl mt-2">{Math.round(temp)}</p>
 
+              <p className="lg:font-bold lg:text-3xl font-semibold text-xl mt-2 ml-1">0</p>
+            </section>
+          )}
+          <h2 className="text-xs sm:text-lg font-bold">{name}</h2>
+        </section>
       </section>
     </div>
   );
